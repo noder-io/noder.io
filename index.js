a8447d0ced006a5b3be1d07599a38aefce6122f5
Nicolab.net <dev@nicolab.net>
2014-03-05 16:17:36 +0100

    Add port by default (3000)

--- index.js
+++ index.js
@@ -578,6 +578,13 @@ noder.conf = loaded.configurator({
     ],
     default: 'development',
     env: 'NODE_ENV'
+  },
+
+  port: {
+    doc: "The port to bind.",
+    format: "port",
+    default: 3000,
+    env: "PORT"
   }
 });
 
